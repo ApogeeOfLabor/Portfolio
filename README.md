b9d7d0f4eb153909cb806a3d3357e149bbafa816
Vladimir <happy.ehrlich@icloud.com>
2021-12-27 22:56:46 +0300

    Let`s go

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+1. Just something utils
+2. Link repositories with telegram bot
---
+++ my_training_utilities/transform_time_interval.py
@@ -0,0 +1,21 @@
+def get_time_interval(sec, templates):
+    result = list()
+    for key, value in templates.items():
+        result.append(f"{sec // value} {key}" if sec // value else '')
+        sec -= value * (sec // value)
+
+    return  result
+
+
+if __name__ == '__main__':
+    dict_templates = {
+        'нед.': 604800,
+        'дн.': 86400,
+        'час.': 3600,
+        'мин.': 60,
+        'сек.': 1,
+    }
+    try:
+        print(*get_time_interval(int(input('Введите колличество секунд: ')), dict_templates))
+    except ValueError:
+        print('Введено неверное значение!')
