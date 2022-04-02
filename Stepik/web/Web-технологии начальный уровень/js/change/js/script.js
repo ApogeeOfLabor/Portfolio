b013b33bc0b382f9ee5ad59e28236dfc9870de28
Vladimir <77030634+ApogeeOfLabor@users.noreply.github.com>
2022-04-02 20:01:27 +0300

    Delete Stepik/web directory

--- Stepik/web/Web-технологии начальный уровень/js/change/js/script.js
+++
@@ -1,6 +0,0 @@
-const inputElement = document.querySelector(".block__text");
-const resultElement = document.querySelector("span");
-
-inputElement.addEventListener("change", function somename(){
-	resultElement.textContent = inputElement.value;
-})
