1bf0ec452ee1954d1869251228fabee8dc0c6819
Vladimir <happy.ehrlich@icloud.com>
2022-04-21 23:10:24 +0300

    Python is temporary stop

--- Django/pg/pg/settings.py
+++ Django/pg/pg/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3_-67-a*_ba(v00cuq5bvo1x-57vvr6cfj)i4b-ddq0p&$qqq0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pass_gen.apps.PassGenConfig'
+    'passgen.apps.PassGenConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Stepik/web/Web_technologes_Start_level/js/Finish_homework_Coffee_House/main.js
+++ Stepik/web/Web_technologes_Start_level/js/Finish_homework_Coffee_House/main.js
@@ -1,8 +1,100 @@
-$('body').on('input', '.menu__count', function () {
-  this.value = this.value.replace(/[^0-9]/g, '');
-});
+const firstname = document.querySelector('.firstname')
+const lastname = document.querySelector('.lastname')
 
+const checkboxes = document.querySelectorAll('[name="product"]');
+const counts = document.querySelectorAll('[type="number"]');
 
 const btn = document.querySelector('.header__submit');
-const checkboxes = document.querySelectorAll('[name="product"]');
+const result = document.querySelector('.result__text')
 
+
+checkboxes.forEach(element => {
+  element.addEventListener("change", function () {
+    if (element.checked) {
+      let count = document.querySelector("[name='count']")
+      count.value = 1;
+    }
+  })
+});
+
+const Products = {
+  espresso: 0,
+  americano: 0,
+  latte: 0,
+  capuchino: 0,
+  choco_cupcake: 0,
+  blueberrie_cupcake: 0,
+  apple_cupcake: 0,
+};
+
+const choicePriceGoods = {
+  expresso: 0,
+  americano: 0,
+  latte: 0,
+  capuchino: 0,
+  chocolate_muffin: 0,
+  blueberry_muffin: 0,
+  apple_tart: 0,
+};
+
+function getResultSum() {
+  let sum = 0;
+  Object.keys(countGoods).forEach((key) => {
+    sum += countGoods[key] * choicePriceGoods[key];
+  });
+  resultElem.innerText = `Итого: ${sum} р.`;
+  return sum;
+}
+
+countElements.forEach((elem) => {
+  elem.addEventListener("change", function () {
+    let goodEl = null;
+    goodsElements.forEach((goods) => {
+      if (this.id === goods.dataset.goods) {
+        goodEl = goods;
+      }
+    });
+    if (
+      this.value < 0 ||
+      this.value.startsWith("0") ||
+      this.value.length === 0
+    ) {
+      goodEl.checked = false;
+      countGoods[this.id] = 0;
+      choicePriceGoods[this.id] = 0;
+      this.value = 0;
+    } else {
+      goodEl.checked = true;
+      countGoods[this.id] = this.value;
+      choicePriceGoods[this.id] = goodEl.value;
+    }
+    getResultSum();
+  });
+});
+
+goodsElements.forEach((product) => {
+  product.addEventListener("change", function () {
+    let countEl = null;
+    countElements.forEach((el) => {
+      if (el.id === this.dataset.goods) {
+        countEl = el;
+      }
+    });
+    if (this.checked) {
+      countGoods[this.dataset.goods] += 1;
+      choicePriceGoods[this.dataset.goods] += this.value;
+      countEl.value = countGoods[this.dataset.goods];
+    } else {
+      countGoods[this.dataset.goods] = 0;
+      choicePriceGoods[this.dataset.goods] = 0;
+      countEl.value = 0;
+    }
+    getResultSum();
+  });
+});
+
+btn.addEventListener("click", () => {
+  alert(
+    `Заказчик: ${userName.value} ${userSurname.value}\n${resultElem.textContent}`
+  );
+});
